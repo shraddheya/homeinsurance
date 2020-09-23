ce722d687f77517154476779d79888a0c169cdf1
sourabh <sourabh95.it@gmail.com>
2020-09-23 16:24:50 +0530

    few changes

--- lemonadeinsurance/src/app/quotes-chighvalueitem/quotes-chighvalueitem.component.ts
+++ lemonadeinsurance/src/app/quotes-chighvalueitem/quotes-chighvalueitem.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit,ViewChild} from '@angular/core';
+import { ModalDirective } from 'angular-bootstrap-md';
 declare var Swal:any;
 @Component({
   selector: 'app-quotes-chighvalueitem',
@@ -6,44 +7,85 @@ declare var Swal:any;
   styleUrls: ['./quotes-chighvalueitem.component.scss']
 })
 export class QuotesChighvalueitemComponent implements OnInit {
+  @ViewChild('highvaluemodal',{static:true}) private highvaluemodal: ModalDirective
   valueItem:any= [
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_jewelry.png",
-      title:"JEWELRY"
+      title:"JEWELRY",
+      text:"What’s the total value of your jewelry items that are worth more than 5.000 € each?",
+      max:"50000"
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_bicycle.png",
-      title:"BICYCLES"
+      title:"BICYCLES",
+      text:"What’s the total value of your bikes that are worth more than 5.000 € each?",
+      max:"10000"
+
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_camera.png",
-      title:"CAMERAS"
+      title:"CAMERAS",
+      text:"What’s the total value of your camera gear worth more than 5.000 € each?",
+      max:"10000"
+
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_portable_electronics.png",
-      title:"PORTABLE ELECTRONICS"
+      title:"PORTABLE ELECTRONICS",
+      text:"What’s the total value of your portable electronics worth more than 5.000 € each?",
+      max:"10000"
+
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_musical_instrument.png",
-      title:"MUSIC GEAR"
+      title:"MUSIC GEAR",
+      text:"What’s the total value of your music gear worth more than 5.000 € each?",
+      max:"10000"
+
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_fashion_items.png",
-      title:"FASHION ITEMS"
+      title:"FASHION ITEMS",
+      text:"What’s the total value of your fashion items worth more than 5.000 € each?",
+      max:"10000"
+
     },
     {
       img:"https://www.lemonade.com/assets/desktop-onboarding/valuable_items_other.png",
-      title:"OTHER"
+      title:"OTHER",
+      text:"What’s the total value of any other items worth more than 5.000 € each?",
+      max:"20000"
+
+    }
+  ];
+  importantArray:any=[
+    {
+      img:'https://www.lemonade.com/assets/desktop-onboarding/email_icon.png',
+      text:"After you get a policy, we'll send you instructions to add your items for our review"
+    },
+    {
+      img:'https://www.lemonade.com/assets/desktop-onboarding/attention-icon.png',
+      text:"Extra Coverage doesn't include wear & tear, equipment malfunctions and zombie attacks."
     }
   ]
+  footerbtnshow:any = true;
+  modalObject:any={};
+  noticeModal:any = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   openModal(data){
-
-    console.log(data)
+    this.footerbtnshow = true;
+    this.noticeModal = false
+    this.modalObject = data;
+    this.highvaluemodal.show();
+  }
+  worthFunction(sign:any , data:any){
+    if(sign.name === "plus"){
+      this.footerbtnshow = false;
+    }
   }
 
 }
