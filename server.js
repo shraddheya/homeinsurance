86bfbb40d57f3cb914464d98a8fee7c71dedca65
Shraddheya <shraddheya.shrivastava@gamil.com>
2020-11-19 01:00:20 +0530

    lets see

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     console.log(`running on port ${port}`);
 });
 
@@ -12,3 +12,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'oursurplus', 'index.html'));
 });
 
+ 
