935b7c4c9d455e656dc3605d8e33f5ae49262069
Shraddheya <shraddheya.shrivastava@gamil.com>
2020-11-19 09:36:09 +0530

    heroku

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-app.use(express.static(__dirname + '/dist'));
+const pth = path.join(__dirname, 'dist', 'oursurplus');
+app.use(express.static(pth));
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);
 });
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'oursurplus', 'index.html'));
+    res.sendFile(path.join(pth, 'index.html'));
 });
 
  
