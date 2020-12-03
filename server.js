4f5d901f694358f6ba0f9858e1becad82d110347
Shraddheya <shraddheya.shrivastava@gamil.com>
2020-12-03 23:52:39 +0530

    must changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const pth = path.join(__dirname, 'dist', 'oursurplus');
 app.use(express.static(pth));
 
