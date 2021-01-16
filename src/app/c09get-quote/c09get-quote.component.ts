61fd7077ef78ad25c631f6aeaf71cf2beb07f56f
sourabh <sourabh95.it@gmail.com>
2021-01-16 12:31:35 +0530

    changes

--- src/app/c09get-quote/c09get-quote.component.ts
+++ src/app/c09get-quote/c09get-quote.component.ts
@@ -10,21 +10,24 @@ import * as moment from 'moment';
   styleUrls: ['./c09get-quote.component.scss']
 })
 export class C09getQuoteComponent implements OnInit {
+  minDate: any = moment().subtract(100, 'year').format('YYYY-MM-DD')
   maxDate: any = this.datepipe.transform(new Date(new Date().getTime() - 6570 * 24 * 60 * 60 * 1000), 'yyyy-MM-dd');
   moreDetails = new FormGroup({
     email: new FormControl(this.ds.allData['c09get-quote'].email, Validators.compose([Validators.required])),
-    dob: new FormControl('', Validators.compose([Validators.required])),
+    dob: new FormControl(this.maxDate, Validators.compose([Validators.required])),
     receivediscount: new FormControl(this.ds.allData['c09get-quote'].receivediscount),
     privacyterm: new FormControl(this.ds.allData['c09get-quote'].privacyterm, Validators.requiredTrue),
   });
-  constructor(public ds: DataterminalService, private datepipe: DatePipe) {}
+  constructor(public ds: DataterminalService, private datepipe: DatePipe) {
+    var years = moment().diff(this.maxDate, 'years', true);
+  }
 
   ngOnInit(): void { }
   clicked() {
     this.ds.allData['c09get-quote'] = this.moreDetails.value;
     this.ds.changepage();
   }
-  validateAge(){
+  validateAge() {
     return false
   }
 }
