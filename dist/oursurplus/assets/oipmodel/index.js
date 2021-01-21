11fab7ca6cbbfc5eadbf795e572c2ea51ebb9a0e
sourabh <sourabh95.it@gmail.com>
2021-01-21 13:27:59 +0530

    latestcode210121

---
+++ dist/oursurplus/assets/oipmodel/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content_lowvalue: require('./content_lowvalue.json'), 
+  personalliability: require('./personalliability.json'), 
+  temporary_accomodation: require('./temporary_accomodation.json'), 
+  jewellery: require('./jewellery.json'), 
+  bicycles: require('./bicycles.json'), 
+  cameras: require('./cameras.json'), 
+  electronics: require('./electronics.json'), 
+  music: require('./music.json'), 
+  other: require('./other.json')
+}
